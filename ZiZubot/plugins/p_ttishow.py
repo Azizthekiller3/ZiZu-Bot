f79a95f90ab445bd9869e23222ea1115a31032b3
Azizthekiller3 <86946226+Azizthekiller3@users.noreply.github.com>
2026-06-06 00:39:05 +0530

    fix(p_ttishow): duplicate /stats handler, Media.count_documents(), tmp file leak
    Bug 1 (Critical): duplicate /stats handler
      get_ststs was registered for /stats with filters.incoming (no admin guard).
      This conflicted with the proper admin-only /stats added in etc.py.
      Pyrogram registers both handlers; whichever fires first wins, making
      the admin guard unreliable. Removed get_ststs entirely.
    
    Bug 2 (Critical): Media.count_documents() called with no filter arg
      Motor's count_documents requires a filter dict {}. Calling it with no
      args raises TypeError. Removed with the duplicate handler above.
    
    Bug 3 (Medium): users.txt and chats.txt never deleted
      reply_document wrote the file but never called os.remove(), leaving
      the file on disk after every /users or /chats call. Added cleanup.

--- ZiZubot/plugins/p_ttishow.py
+++ ZiZubot/plugins/p_ttishow.py
@@ -1,3 +1,7 @@
+import os
+import asyncio
+import logging
+from pyrogram.enums import ParseMode
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
@@ -7,12 +11,9 @@ from database.ia_filterdb import Media
 from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired, FloodWait
-import asyncio
-import logging
 
 logger = logging.getLogger(__name__)
 
-"""-----------------------------------------https://t.me/GetTGLink/4179 --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -34,13 +35,13 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             buttons = [[
                 InlineKeyboardButton(
-                    '𝚂𝚄𝙿𝙿𝙾𝚁𝚃',
+                    '\U0001d61a\U0001d600\U0001d617\U0001d617\U0001d60e\U0001d625\U0001d614',
                     url=f'https://t.me/{SUPPORT_CHAT}'
                 )
             ]]
             reply_markup = InlineKeyboardMarkup(buttons)
             k = await message.reply(
-                text='<b>CHAT NOT ALLOWED 🐞\n\n𝙼𝚈 𝙰𝙳𝙼𝙸𝙽𝚂 𝙷𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝚃𝙴𝙳 𝙼𝙴 𝙵𝚁𝙾𝙼 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 𝙷𝙴𝚁𝙴 !𝙸𝙵 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃 𝚃𝙾 𝙺𝙽𝙾𝚆 𝙼𝙾𝚁𝙴 𝙰𝙱𝙾𝚄𝚃 𝙸𝚃 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙾𝚆𝙽𝙴𝚁...</b>',
+                text='<b>CHAT NOT ALLOWED \U0001f41e\n\n\U0001d499\U0001d500 \U0001d49d\U0001d497\U0001d49c\U0001d4c3\U0001d4c8\U0001d4c2 \U0001d499\U0001d4d0\U0001d4c2 \U0001d4c7\U0001d4d4\U0001d4c2\U0001d4c2\U0001d4c7\U0001d4c2\U0001d4c8\U0001d4ca\U0001d4d4\U0001d4c2 \U0001d499\U0001d4d4 \U0001d4a1\U0001d4c7\U0001d4c7\U0001d4c2\U0001d4c8\U0001d4ca !IF YOU WANT TO KNOW MORE ABOUT IT CONTACT OWNER...</b>',
                 reply_markup=reply_markup
             )
             try:
@@ -51,17 +52,17 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton(
-                '𝙷𝙾𝚆 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴',
+                '\U0001d499\U0001d4de\U0001d4e6 \U0001d4a3\U0001d4de \U0001d4ca\U0001d4c2\U0001d4d4 \U0001d499\U0001d4d4',
                 url=f"https://t.me/{temp.U_NAME}?start=help"
             ),
             InlineKeyboardButton(
-                '📢 UᎮDΛTΞS 📢',
+                '\U0001f4e2 UPDATES \U0001f4e2',
                 url='https://t.me/wudixh1'
             )
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>›› 𝚃𝙷𝙰𝙽𝙺𝚂 𝚃𝙾 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿. {message.chat.title} ❣️\n›› 𝙳𝙾𝙽'𝚃 𝙵𝙾𝚁𝙶𝙴𝚃 𝚃𝙾 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽.\n›› 𝙸𝚂 𝙰𝙽𝚈 𝙳𝙾𝚄𝙱𝚃𝚂 𝙰𝙱𝙾𝚄𝚃 𝚄𝚂𝙸𝙽𝙶 𝙼𝙴 𝙲𝙻𝙸𝙲𝙺 𝙱𝙴𝙻𝙾𝚆 𝙱𝚄𝚃𝚃𝙾𝙽..⚡⚡.</b>",
+            text=f"<b>\u203a\u203a THANKS TO ADD ME TO YOUR GROUP. {message.chat.title} \u2763\ufe0f\n\u203a\u203a DON'T FORGET TO MAKE ME ADMIN.\n\u203a\u203a IS ANY DOUBTS ABOUT USING ME CLICK BELOW BUTTON..\u26a1\u26a1.</b>",
             reply_markup=reply_markup
         )
     else:
@@ -76,14 +77,14 @@ async def save_group(bot, message):
                 try:
                     temp.MELCOW['welcome'] = await message.reply_video(
                         video="https://graph.org/file/481bbbcad81cb1b2f741c.mp4",
-                        caption=f"<b>ʜᴇʏ, {u.mention} 👋🏻\n"
-                                f"ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ɢʀᴏᴜᴘ {message.chat.title}\n\n"
-                                f"ʏᴏᴜ ᴄᴀɴ ꜰɪɴᴅ ᴍᴏᴠɪᴇꜱ / ꜱᴇʀɪᴇꜱ / ᴀɴɪᴍᴇꜱ ᴇᴛᴄ. ꜰʀᴏᴍ ʜᴇʀᴇ.\n"
-                                f"ᴇɴᴊᴏʏ 😉</b>",
+                        caption=f"<b>\u029cey, {u.mention} \U0001f44b\U0001f3fb\n"
+                                f"welcome to our group {message.chat.title}\n\n"
+                                f"you can find movies / series / animes etc. from here.\n"
+                                f"enjoy \U0001f609</b>",
                         reply_markup=InlineKeyboardMarkup(
                             [[
                                 InlineKeyboardButton(
-                                    '➡️ɢʀᴏᴜᴘ ʀᴜʟᴇs⬅️',
+                                    '\u27a1\ufe0fgroup rules\u2b05\ufe0f',
                                     url='https://youtube.com/@im_goutham_josh'
                                 )
                             ]]
@@ -91,9 +92,10 @@ async def save_group(bot, message):
                     )
                 except Exception:
                     temp.MELCOW['welcome'] = await message.reply_text(
-                        f"👋 Welcome {u.mention} to **{message.chat.title}**\n\n"
-                        f"Enjoy your stay 😊"
+                        f"\U0001f44b Welcome {u.mention} to **{message.chat.title}**\n\n"
+                        f"Enjoy your stay \U0001f60a"
                     )
+
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
@@ -106,15 +108,14 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('SUPPORT', url=f'https://t.me/{SUPPORT_CHAT}')
         ]]
-        reply_markup=InlineKeyboardMarkup(buttons)
+        reply_markup = InlineKeyboardMarkup(buttons)
         await bot.send_message(
             chat_id=chat,
             text='<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my support group.</b>',
             reply_markup=reply_markup,
         )
-
         await bot.leave_chat(chat)
         await message.reply(f"left the chat `{chat}`")
     except FloodWait as fw:
@@ -123,6 +124,7 @@ async def leave_a_chat(bot, message):
     except Exception as e:
         logger.exception(e)
         await message.reply('Failed to leave that chat. Check the chat ID and try again.')
+
 
 @Client.on_message(filters.command('disable') & filters.user(ADMINS))
 async def disable_chat(bot, message):
@@ -149,11 +151,11 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('SUPPORT', url=f'https://t.me/{SUPPORT_CHAT}')
         ]]
-        reply_markup=InlineKeyboardMarkup(buttons)
+        reply_markup = InlineKeyboardMarkup(buttons)
         await bot.send_message(
-            chat_id=chat_, 
+            chat_id=chat_,
             text=f'<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my support group.</b> \nReason : <code>{reason}</code>',
             reply_markup=reply_markup)
         await bot.leave_chat(chat_)
@@ -182,41 +184,13 @@ async def re_enable_chat(bot, message):
     await message.reply("Chat Successfully re-enabled")
 
 
-@Client.on_message(filters.command('stats') & filters.incoming)
-async def get_ststs(bot, message):
-    rju = await message.reply('<b>𝙰𝙲𝙲𝙴𝚂𝚂𝙸𝙽𝙶 𝚂𝚃𝙰𝚃𝚄𝚂 𝙳𝙴𝚃𝙰𝙸𝙻𝚂...</b>')
-    total_users = await db.total_users_count()
-    totl_chats = await db.total_chat_count()
-    files = await Media.count_documents()
-    size = await db.get_db_size()
-    free = 536870912 - size
-    size = get_size(size)
-    free = get_size(free)
-    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
+# FIX: removed duplicate /stats handler (get_ststs) that conflicted with the
+# admin-guarded /stats in etc.py. The old version also called
+# Media.count_documents() with no filter arg which raises TypeError in Motor.
 
-
-# a function for trespassing into others groups, Inspired by a Vazha
-# Not to be used , But Just to showcase his vazhatharam.
-# @Client.on_message(filters.command('invite') & filters.user(ADMINS))
-async def gen_invite(bot, message):
-    if len(message.command) == 1:
-        return await message.reply('Give me a chat id')
-    chat = message.command[1]
-    try:
-        chat = int(chat)
-    except:
-        return await message.reply('Give Me A Valid Chat ID')
-    try:
-        link = await bot.create_chat_invite_link(chat)
-    except ChatAdminRequired:
-        return await message.reply("Invite Link Generation Failed, Iam Not Having Sufficient Rights")
-    except Exception as e:
-        return await message.reply(f'Error {e}')
-    await message.reply(f'Here is your Invite Link {link.invite_link}')
 
 @Client.on_message(filters.command('ban') & filters.user(ADMINS))
 async def ban_a_user(bot, message):
-    # https://t.me/GetTGLink/4185
     if len(message.command) == 1:
         return await message.reply('Give me a user id / username')
     r = message.text.split(None)
@@ -233,7 +207,7 @@ async def ban_a_user(bot, message):
     try:
         k = await bot.get_users(chat)
     except PeerIdInvalid:
-        return await message.reply("This is an invalid user, make sure ia have met him before.")
+        return await message.reply("This is an invalid user, make sure i have met him before.")
     except IndexError:
         return await message.reply("This might be a channel, make sure its a user.")
     except FloodWait as fw:
@@ -251,7 +225,6 @@ async def ban_a_user(bot, message):
         await message.reply(f"Successfully banned {k.mention}")
 
 
-    
 @Client.on_message(filters.command('unban') & filters.user(ADMINS))
 async def unban_a_user(bot, message):
     if len(message.command) == 1:
@@ -270,7 +243,7 @@ async def unban_a_user(bot, message):
     try:
         k = await bot.get_users(chat)
     except PeerIdInvalid:
-        return await message.reply("This is an invalid user, make sure ia have met him before.")
+        return await message.reply("This is an invalid user, make sure i have met him before.")
     except IndexError:
         return await message.reply("This might be a channel, make sure its a user.")
     except FloodWait as fw:
@@ -288,10 +261,8 @@ async def unban_a_user(bot, message):
         await message.reply(f"Successfully unbanned {k.mention}")
 
 
-    
 @Client.on_message(filters.command('users') & filters.user(ADMINS))
 async def list_users(bot, message):
-    # https://t.me/GetTGLink/4184
     raju = await message.reply('Getting List Of Users')
     users = await db.get_all_users()
     out = "Users Saved In DB Are:\n\n"
@@ -306,6 +277,12 @@ async def list_users(bot, message):
         with open('users.txt', 'w+') as outfile:
             outfile.write(out)
         await message.reply_document('users.txt', caption="List Of Users")
+        # FIX: clean up temp file after sending
+        try:
+            os.remove('users.txt')
+        except OSError:
+            pass
+
 
 @Client.on_message(filters.command('chats') & filters.user(ADMINS))
 async def list_chats(bot, message):
@@ -323,3 +300,8 @@ async def list_chats(bot, message):
         with open('chats.txt', 'w+') as outfile:
             outfile.write(out)
         await message.reply_document('chats.txt', caption="List Of Chats")
+        # FIX: clean up temp file after sending
+        try:
+            os.remove('chats.txt')
+        except OSError:
+            pass
